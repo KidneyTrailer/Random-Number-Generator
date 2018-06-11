f4ed3cf700ca51890a52b44594d67c635c46bdea
KidneyTrailer <ngorden1997@gmail.com>
2018-06-11 12:06:47 -0700

    Added SpecFlow Unit Testing for Deck.GetLength()

---
+++ RNFC.SpecFlow.Tests/DeckLengthSteps.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomNumsFromCards;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RNFC.SpecFlow.Tests
+{
+    [Binding]
+    public class DeckLengthSteps
+    {
+        Deck testDeck;
+        int result;
+
+        [Given(@"I have two decks")]
+        public void GivenIHaveTwoDecks()
+        {
+            testDeck = new Deck(2);
+        }
+        
+        [When(@"I see how many cards I have")]
+        public void WhenISeeHowManyCardsIHave()
+        {
+            result = testDeck.GetLength();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int amount)
+        {
+            Assert.AreEqual(amount, result);
+        }
+    }
+}
