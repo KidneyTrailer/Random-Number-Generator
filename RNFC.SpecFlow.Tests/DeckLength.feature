22d9689ebb1e91920cc842b40fc8cb9e6457cb3e
KidneyTrailer <ngorden1997@gmail.com>
2018-06-11 12:06:47 -0700

    Added SpecFlow Unit Testing for Deck.GetLength()

---
+++ RNFC.SpecFlow.Tests/DeckLength.feature
@@ -0,0 +1,10 @@
+﻿Feature: DeckLength
+	In order to verify the number of decks can vary
+	As a person seeking random numbers
+	I want to be able to draw from multiple decks
+
+@mytag
+Scenario: Two Decks
+	Given I have two decks
+	When I see how many cards I have
+	Then the result should be 104
---
+++ RNFC.SpecFlow.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RNFC.SpecFlow.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RNFC.SpecFlow.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4562851-3d9a-4c86-8823-1a7fc2956280")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
